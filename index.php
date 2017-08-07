a7ec471f460707a4ef85585e23ae8c8b8c3ba5c7
jaggesher <jaggesher14@gmail.com>
2017-08-07 23:56:05 +0600

    Cleaning Code

--- index.php
+++ index.php
@@ -274,66 +274,7 @@
   <p>Developed By Md. Abu Musa (PGD 2016~17)</p>
 </footer>
 
-<script>
-$(document).ready(function(){
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        window.location.hash = hash;
-      });
-    } 
-  });
-  
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
-  });
-   $(document).on('click', '.browse', function() {
-      var file = $(this).parent().parent().parent().find('.file');
-      file.trigger('click');
-  });
-  $(document).on('change', '.file', function() {
-      $("#displayFileName").val($(this).val().replace(/C:\\fakepath\\/i, ''));
-  });
-
-  $(document).on('click','#search',function(){
-    var Code=$("#code").val();
-    if(Code!=""){
-      $.post( "deleteIt.php",
-            {Code: Code,Action: "Send"},
-            function(data) {
-                $("#DeletingData").html(data);
-            },"html");
-    }else{
-      alert("Please Fill All Info.");
-    }
-    
-  });
-  $(document).on('click','#delete',function(){
-    var Code=$("#code").val();
-    if(Code!=""){
-      $.post( "deleteIt.php",
-            {Code: Code,Action: "delete"},
-            function(data) {
-                $("#DeletingData").html(data);
-            },"html");
-    }else{
-      alert("Please Fill All Info.");
-    }
-  });
-});
-</script>
+<script src="JS/index.js"></script>
 
 </body>
 </html>
